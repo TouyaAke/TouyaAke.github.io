77d70ff339784bd0961151a7c47b361efccab954
TouyaAke <134456909+TouyaAke@users.noreply.github.com>
2024-08-12 16:31:08 -0400

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 30,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        620: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+
+    }
+});
